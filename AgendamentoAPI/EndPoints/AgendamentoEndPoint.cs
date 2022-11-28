354af24b03eaf99fc0ae3b6e5095e1fa00e83098
MATHEUS CARNIZELLA <matheus.cne12@gmail.com>
2022-11-28 13:12:26 -0300

    @MatheusCarnizella Agendamento EndPoints

--- AgendamentoAPI/EndPoints/AgendamentoEndPoint.cs
+++ AgendamentoAPI/EndPoints/AgendamentoEndPoint.cs
@@ -7,14 +7,20 @@ namespace AgendamentoAPI.EndPoints
     {
         public static void MapAgendamentoEndPoint(this WebApplication ep)
         {
-            ep.MapPost("/agendamento/Agenedar", (Agendamento agendamento, IAgendamentoRepository repository) =>
+            ep.MapPost("/agendamento/Agendar", (Agendamento agendamento, IAgendamentoRepository repository) =>
             {
                 repository.Post(agendamento);
                 return Results.Created($"/Agendar/{agendamento.agendamentoId}", agendamento);
             })
                 .Produces<Agendamento>(StatusCodes.Status201Created)
-                .WithName("Agendar")
+                .WithName("AgendarPaciente")
                 .WithTags("Agendar");
+
+            ep.MapGet("/agendamento/pegartodosAgendamentos", async (IAgendamentoRepository repository) =>
+            await repository.GetAll())
+           .Produces<List<Agendamento>>(StatusCodes.Status200OK)
+           .WithName("MostrarAgendamentos")
+           .WithTags("Agendar");
         } 
     }
 }
